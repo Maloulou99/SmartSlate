d123ff078a3f44b03fbd8e08da75a8085b27ca6a
Maloulou99 <baha0610@stud.kea.dk>
2023-04-26 16:18:53 +0200

    Sql rettet og tilføjet tabeller

---
+++ src/main/resources/sql/smartslate_database.sql
@@ -0,0 +1,77 @@
+-- Create database if no one exist
+CREATE DATABASE IF NOT EXISTS smartslate_database;
+
+-- Use database
+USE smartslate_database;
+
+-- Drop tables if they exist
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employeeTasks;
+
+
+-- Create new tables
+CREATE TABLE Users
+(
+    UserID    INT PRIMARY KEY,
+    Username  VARCHAR(255) NOT NULL,
+    Password  VARCHAR(255) NOT NULL,
+    Email     VARCHAR(255),
+    FirstName VARCHAR(255),
+    LastName  VARCHAR(255),
+    CreatedAt DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE Projects
+(
+    ProjectID   INT PRIMARY KEY,
+    Name        VARCHAR(255) NOT NULL,
+    Description VARCHAR(1000),
+    StartDate   DATE         NOT NULL,
+    EndDate     DATE,
+    Budget      DECIMAL(10, 2),
+    Status      VARCHAR(20)  NOT NULL
+);
+
+-- Når ON DELETE CASCADE er angivet for en fremmednøgle i en tabel,
+-- betyder det, at hvis en række i den tilsvarende primærnøgletabel slettes,
+-- vil alle de tilknyttede rækker i den tilknyttede tabel også blive slettet automatisk.
+CREATE TABLE Tasks
+(
+    TaskID      INT PRIMARY KEY,
+    ProjectID   INT          NOT NULL,
+    Name        VARCHAR(255) NOT NULL,
+    Description VARCHAR(1000),
+    StartDate   DATE         NOT NULL,
+    EndDate     DATE,
+    Budget      DECIMAL(10, 2),
+    Status      VARCHAR(20)  NOT NULL,
+    CONSTRAINT fk_project FOREIGN KEY (ProjectID) REFERENCES Projects (ProjectID) ON DELETE CASCADE
+);
+
+CREATE TABLE Employees
+(
+    EmployeeID INT PRIMARY KEY,
+    FirstName  VARCHAR(255) NOT NULL,
+    LastName   VARCHAR(255) NOT NULL,
+    Email      VARCHAR(255),
+    Phone      VARCHAR(20)
+);
+
+-- En CONSTRAINT kan for eksempel sikre, at ingen værdier i en bestemt kolonne kan være NULL,
+-- eller at værdierne i en kolonne skal være unikke. Derudover kan CONSTRAINT bruges til at definere og
+-- styre relationerne mellem tabeller i en database ved at specificere primærnøgler,
+-- fremmednøgler og deres henvisningsbegrænsninger.
+
+CREATE TABLE EmployeeTasks
+(
+    EmployeeID INT NOT NULL,
+    TaskID     INT NOT NULL,
+    CONSTRAINT pk_employee_task PRIMARY KEY (EmployeeID, TaskID),
+    CONSTRAINT fk_employee FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID) ON DELETE CASCADE,
+    CONSTRAINT fk_task FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID) ON DELETE CASCADE
+);
