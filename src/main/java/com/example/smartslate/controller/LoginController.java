c821cb08709135fa5fb0e2578eca5c59d0c94251
Maloulou99 <baha0610@stud.kea.dk>
2023-05-04 15:07:24 +0200

    application.properties tilrettet

--- src/main/java/com/example/smartslate/controller/LoginController.java
+++ src/main/java/com/example/smartslate/controller/LoginController.java
@@ -1,73 +1,48 @@
 package com.example.smartslate.controller;
 
 import com.example.smartslate.model.User;
-import com.example.smartslate.repository.LoginRepository;
-import com.example.smartslate.repository.ProjectRepository;
-import com.example.smartslate.repository.UserRepository;
 import com.example.smartslate.service.LoginService;
-import com.example.smartslate.service.ProjectService;
-import com.example.smartslate.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("/")
 @Controller
 public class LoginController {
-    private LoginService loginService;
-    private ProjectService projectService;
-    private UserService userService;
-    private int currentUser;
+    private final LoginService loginService;
 
-    public LoginController(LoginService loginService, ProjectService projectService, UserService userService) {
+    public LoginController(LoginService loginService) {
         this.loginService = loginService;
-        this.projectService = projectService;
-        this.userService = userService;
     }
 
-    protected boolean isLoggedIn(HttpSession session, int uid) {
-        return session.getAttribute("user") != null && currentUser == uid;
-    }
-
-    @GetMapping()
-    public String landingPage(Model model) {
-        model.addAttribute("loggedIn", true);
-        return "index";
-    }
-
-    // User login
-    @GetMapping("/user/login")
-    public String showUserLogin() {
+    @GetMapping("/login")
+    public String showLoginForm() {
         return "user-login";
     }
 
-    @PostMapping("/user/login")
-    public String login(@RequestParam String usernameOrEmail, @RequestParam String password, HttpSession session) {
+    @PostMapping("/login")
+    public String login(@RequestParam String usernameOrEmail, @RequestParam String password, HttpSession session, Model model) {
         User user = null;
         if (usernameOrEmail.contains("@")) {
-            user = loginService.findByUsernameAndPassword(usernameOrEmail, password);
-        } else {
             user = loginService.findByEmailAndPassword(usernameOrEmail, password);
+        } else {
+            user = loginService.findByUsernameAndPassword(usernameOrEmail, password);
         }
+
         if (user != null) {
             session.setAttribute("userId", user.getUserId());
-            return "redirect:/main-page/" + user.getUserId();
+            return "redirect:/dashboard";
         } else {
+            model.addAttribute("error", "Invalid login credentials");
             return "user-login";
         }
     }
 
     @GetMapping("/logout")
-    public String logout(HttpSession session, Model model) {
+    public String logout(HttpSession session) {
         session.invalidate();
-        model.addAttribute("loggedIn", false);
-        return "index";
+        return "redirect:/user-login";
     }
-
-
 }
-
--- src/main/java/com/example/smartslate/utility/FileHandler.java
+++ src/main/java/com/example/smartslate/utility/FileHandler.java
@@ -1,4 +1,4 @@
-package com.example.smartslate.repository;
+package com.example.smartslate.utility;
 
 import java.io.File;
 import java.io.IOException;
