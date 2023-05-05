ebcf85ca058c90aae6f2bbd187d4cb7232188663
Maloulou99 <baha0610@stud.kea.dk>
2023-05-05 14:19:20 +0200

    Login metode rettet

--- src/main/java/com/example/smartslate/controller/LoginController.java
+++ src/main/java/com/example/smartslate/controller/LoginController.java
@@ -1,26 +1,45 @@
 package com.example.smartslate.controller;
 
 import com.example.smartslate.model.User;
+import com.example.smartslate.repository.LoginRepository;
+import com.example.smartslate.repository.ProjectRepository;
+import com.example.smartslate.repository.UserRepository;
 import com.example.smartslate.service.LoginService;
+import com.example.smartslate.service.ProjectService;
+import com.example.smartslate.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RequestMapping("/")
 @Controller
 public class LoginController {
     private LoginService loginService;
-    private int current_userId;
+    private ProjectService projectService;
+    private UserService userService;
+    private int currentUser;
 
-    public LoginController(LoginService loginService) {
+    public LoginController(LoginService loginService, ProjectService projectService, UserService userService) {
         this.loginService = loginService;
+        this.projectService = projectService;
+        this.userService = userService;
     }
 
     protected boolean isLoggedIn(HttpSession session, int uid) {
-        return session.getAttribute("user") != null && current_userId == uid;
+        return session.getAttribute("user") != null && currentUser == uid;
     }
+
+    @GetMapping()
+    public String landingPage(Model model) {
+        model.addAttribute("loggedIn", true);
+        return "index";
+    }
+
 
     @GetMapping("/login")
     public String showLoginForm() {
